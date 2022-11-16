48b9d822356017342d8627e4ac3f6a31a22f0e31
vinicius-hora <vinicius_hora@live.com>
2022-11-16 12:45:55 -0300

    database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsmeta.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+
+
+}
